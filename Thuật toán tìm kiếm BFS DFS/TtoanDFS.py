ba2ea43da5a557255fca754614729162cd9b238e
vanzkiro <loveyouanh0147@gmail.com>
2026-08-11 20:42:31 +0700

    First commit

---
+++ Thuật toán tìm kiếm BFS DFS/TtoanDFS.py
@@ -0,0 +1,50 @@
+def depth_first_search(graph, start_node, goal_node):
+    # 1. Khởi tạo danh sách L chỉ chứa trạng thái ban đầu
+    L = [start_node]
+    father = {}
+    
+    print("=== TÌM KIẾM THEO CHIỀU SÂU (DFS) ===")
+    stt = 1
+    
+    # 2. loop do
+    while True:
+        # 2.1. if L rỗng then {thông báo thất bại; stop}
+        if not L:
+            print("Thông báo: Tìm kiếm thất bại!")
+            return None
+        
+        # 2.2. Loại trạng thái u ở đầu danh sách L
+        u = L.pop(0)
+        print(f"Bước {stt}: u = {u} | L còn lại = {L}")
+        stt += 1
+        
+        # 2.3. if u là trạng thái kết thúc then {thông báo thành công; stop}
+        if u == goal_node:
+            print("Thông báo: Tìm kiếm thành công!")
+            return father
+        
+        # 2.4. for mỗi trạng thái v kề u do {Đặt v vào đầu danh sách L}
+        neighbors = graph.get(u, [])
+        # Đảo ngược danh sách kề để khi chèn vào đầu L, đỉnh trái nhất sẽ đứng ở vị trí đầu tiên
+        for v in reversed(neighbors):
+            L.insert(0, v)     # Đặt v vào đầu danh sách L
+            father[v] = u      # father(v) <- u
+
+
+graph_data = {
+    'A': ['B', 'C', 'D'],
+    'B': ['E', 'F'],
+    'C': ['G', 'H'],
+    'D': ['I', 'J'],
+    'E': ['K', 'L'],
+    'F': ['L', 'M'],
+    'G': ['N'],
+    'H': ['O', 'P'],
+    'I': ['P', 'Q'],
+    'J': ['N'],
+    'K': ['S'],
+    'L': ['T'],
+    'P': ['U']
+}
+
+father_result = depth_first_search(graph_data, start_node='A', goal_node='P')
